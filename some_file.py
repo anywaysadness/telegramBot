41f80ff3bb9889e4ef28df79c86c3452c19b08c2
Maxim Kyrdyumov <oito07@abk.local>
2023-03-01 16:39:21 +0300

    образ телеграм бота

--- some_file.py
+++ some_file.py
@@ -1,2 +1,33 @@
-print("hello world")
-newprint = 1
+import requests
+import time
+
+
+API_URL: str = 'https://api.telegram.org/bot'
+API_CATS_URL: str = 'https://aws.random.cat/meow'
+BOT_TOKEN: str = '6238402712:AAFisyL1qo3AZlnomJkRQ0uI4Dw-FFiCWWM'
+ERROR_TEXT: str = 'Здесь должна быть картинка с котиком!'
+MAX_COUNTER = 10
+
+offset: int = -2
+counter: int = 0
+cat_response: requests.Response
+cat_link: str
+
+
+while counter < MAX_COUNTER:
+    print('attempt =', counter)  #Чтобы видеть в консоли, что код живет
+    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
+
+    if updates['result']:
+        for result in updates['result']:
+            offset = result['update_id']
+            chat_id = result['message']['from']['id']
+            cat_response = requests.get(API_CATS_URL)
+            if cat_response.status_code == 200:
+                cat_link = cat_response.json()['file']
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={cat_link}')
+            else:
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={ERROR_TEXT}')
+
+    time.sleep(1)
+    counter += 1
